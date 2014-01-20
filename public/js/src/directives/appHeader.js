2574088d280d7e1b9a7824b47feb2db99a8e2002
James Hageman <jamesd@hageman.ca>
2014-01-19 20:59:06 -0500

    Added responsive nav

--- public/js/src/directives/appHeader.js
+++ public/js/src/directives/appHeader.js
@@ -1,6 +1,18 @@
 angular.module('app').directive('appHeader', function () {
 	return {
 		restrict: 'A',
-		templateUrl: 'templates/header.html'
+		templateUrl: 'templates/header.html',
+		link: function (scope, elem) {
+			var $nav = elem.find('nav'),
+				nav;
+
+			nav = responsiveNav('#nav', {
+				customToggle: '#nav-toggle'
+			});
+
+			$nav.find('a').on('click', function () {
+				nav.toggle();
+			});
+		}
 	};
 });
