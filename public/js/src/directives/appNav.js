fdf279dfac448e17d32c1e5d8a86d81a5b730850
James Hageman <jamesd@hageman.ca>
2014-01-23 12:40:08 -0500

    Added appNav.js and appnav.html

---
+++ public/js/src/directives/appNav.js
@@ -0,0 +1,28 @@
+angular.module('app').directive('appNav', function () {
+	return {
+		restrict: 'A',
+		templateUrl: 'templates/appnav.html',
+		transclude: true,
+		scope: true,
+		link: function (scope, elem) {
+			var smallNav = elem.find('.small-nav'),
+				largeNav = elem.find('.large-nav'),
+				toggle = elem.find('.nav-toggle');
+
+			smallNav.find('ul').addClass('nav-stacked');
+			smallNav.click('a', function () {
+				smallNav.addClass('closed');
+			});
+
+			toggle.on('click', function () {
+				smallNav.toggleClass('closed');
+			});
+
+			toggle.on('touchend', function (e) {
+				smallNav.toggleClass('closed');
+				e.preventDefault();
+				e.stopPropagation();
+			});
+		}
+	};
+});
