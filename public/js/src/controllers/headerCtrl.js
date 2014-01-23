2e646040d7b9b955d309aaf1b9689179cc4b1aaa
James Hageman <jamesd@hageman.ca>
2014-01-23 12:38:38 -0500

    Seperated header and nav, polished nav functionality

--- public/js/src/controllers/headerCtrl.js
+++ public/js/src/controllers/headerCtrl.js
@@ -1,7 +1,6 @@
 angular.module('app').controller('headerCtrl',
 function ($scope, User, $location) {
 	$scope.handleLogoutButtonClick = function () {
-		alert('logout');
 		User.logout({}, function () {
 			$location.url('/login');
 		});
